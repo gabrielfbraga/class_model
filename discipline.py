86a741c350bf5bf6b0192e96427fe33b9d171edb
Gabriel Freitas Braga <gfbr.snf24@uea.edu.br>
2024-11-17 18:53:08 -0400

    adding disciplines

--- discipline.py
+++ discipline.py
@@ -1,3 +1,14 @@
 class Disciplines:
     def __init__(self):
-        pass
+        self.disciplinas = [
+            "Matemática Discreta", "Algoritmos e Estruturas de Dados", "Redes de Computadores",
+            "Cálculo 1", "Cálculo 2", "Cálculo 3", "Programação Orientada a Objetos", "Banco de Dados",
+            "Engenharia de Software", "Sistemas Operacionais", "Teoria dos Grafos", "Inteligência Artificial",
+            "Redes Neurais", "Compiladores", "Arquitetura de Computadores", "Linguagens Formais e Autômatos",
+            "Teoria da Computação", "Processamento de Imagens", "Desenvolvimento Web", "Segurança da Informação",
+            "Sistemas Distribuídos", "Interfaces de Usuário", "Prática em Programação", "Ética em TI", "Gestão de Projetos de TI"
+        ]
+    
+    def __str__(self):
+        # Retorna a lista de disciplinas como uma string formatada
+        return "\n".join(self.disciplinas)
--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from university import University
 from department import Department
 from teacher import Teacher
 from discipline import Disciplines
+import random
 
 # criando uma instância para as classes
 uni = University("Escola Superior de Tecnologia", "Amazonas")
-dep = Department("Departamentos:")
+dep = Department("\nProfessores:")
 prof = Teacher()
+
+# Criando uma instância da classe Disciplines
+disciplinas = Disciplines()
+
 
 # exibindo as informações da universidade
 print(f"Universidade: {uni.name}, Localização: {uni.location}")
@@ -32,13 +37,28 @@ for dept in uni.list_departments():
     print(f"- {dept}")
 
 print(dep.name)
-dep.add_teacher(type("Teacher", (), {"name": "Jucimar"}))
-dep.add_teacher(type("Teacher", (), {"name": "Elloa"}))
-dep.add_teacher(type("Teacher", (), {"name": "Ponciano"}))
-dep.add_teacher(type("Teacher", (), {"name": "Anderson"}))
-dep.add_teacher(type("Teacher", (), {"name": "Ponciano"}))
+dep.add_teacher(type("Teacher", (), {"name": "Jucimar Maia"}))
+dep.add_teacher(type("Teacher", (), {"name": "Elloa Baareto"}))
+dep.add_teacher(type("Teacher", (), {"name": "Luís Cuevas"}))
+dep.add_teacher(type("Teacher", (), {"name": "Polliany Almeida"}))
+dep.add_teacher(type("Teacher", (), {"name": "Ricardo Barboza"}))
 
 for dept in dep.list_teachers():
     print(f"- {dept}")
+
+
+# Função para atribuir disciplinas
+def atribuir_disciplinas_para_professores():
+    for professor in prof.lista_professores:
+        # Seleciona aleatoriamente 5 disciplinas
+        import random
+        professor_disciplinas = random.sample(disciplinas.disciplinas, 5)
+        print(f"\nProfessor {professor} foi atribuído a seguinte lista de disciplinas:")
+        for disciplina in professor_disciplinas:
+            print(f"- {disciplina}")
+
+# Atribuindo disciplinas para os professores
+atribuir_disciplinas_para_professores()
+
 
 
